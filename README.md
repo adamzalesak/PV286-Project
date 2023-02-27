1401f3620e1242ae5d8e5c91407308ff90dfeb5b
Filip Hájek <filip.hajek@notino.com>
2023-02-27 22:50:56 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Panbyte
+
+TODO
---
+++ tests/Panbyte.Tests/PanbyteTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Panbyte.Tests;
+
+public class PanbyteTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
