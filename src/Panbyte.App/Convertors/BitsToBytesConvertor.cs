2e2528fc9eb54d960962eb598d0e5a4ad4e35b6f
Katarína Platková <katkap253@gmail.com>
2023-03-13 22:35:01 +0100

    feature/bits-to-bytes-convert

--- src/Panbyte.App/Convertors/BitsToBytesConvertor.cs
+++ src/Panbyte.App/Convertors/BitsToBytesConvertor.cs
@@ -7,8 +7,27 @@ public class BitsToBytesConvertor : Convertor
     }
 
     public override Stream ConvertPart(byte[] source)
-    {
-        throw new NotImplementedException();
+    {   
+        var sourceString = System.Text.Encoding.ASCII.GetString(source);
+        var remainder = source.Length % 8;
+        var stream = new MemoryStream();
+        byte oneByte;
+
+        if (remainder != 0)
+        {
+            oneByte = System.Convert.ToByte(sourceString[..remainder], 2);
+            stream.WriteByte(oneByte);
+        }
+
+        for (int i = remainder; i < source.Length; i += 8)
+        {
+            oneByte = System.Convert.ToByte(sourceString.Substring(i, 8), 2);
+            stream.WriteByte(oneByte);
+        }
+
+        stream.Flush();
+        stream.Position = 0;
+        return stream;
     }
 
     public override bool ValidateOptions(out string errorMessage)
