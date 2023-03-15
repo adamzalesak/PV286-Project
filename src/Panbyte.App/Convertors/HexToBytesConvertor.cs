448c29685c514a1a2a1f8b64331ace1d8145c522
Katarína Platková <katkap253@gmail.com>
2023-03-15 18:11:17 +0100

    feature/hex-to-bytes-convert

---
+++ src/Panbyte.App/Convertors/HexToBytesConvertor.cs
@@ -0,0 +1,36 @@
+﻿namespace Panbyte.App.Convertors;
+
+public class HexToBytesConvertor : Convertor
+{
+    public HexToBytesConvertor(ConvertorOptions convertorOptions) : base(convertorOptions)
+    {
+    }
+
+    public override Stream ConvertPart(byte[] source)
+    {
+        var sourceString = System.Text.Encoding.ASCII.GetString(source);
+        var stream = new MemoryStream();
+        byte oneByte;
+
+        if (source.Length % 2 != 0)
+        {
+            throw new ArgumentException("Invalid input value");
+        }
+
+        for (int i = 0; i < source.Length; i += 2)
+        {
+            oneByte = System.Convert.ToByte(sourceString.Substring(i, 2), 16);
+            stream.WriteByte(oneByte);
+        }
+
+        stream.Flush();
+        stream.Position = 0;
+        return stream;
+    }
+
+    public override bool ValidateOptions(out string errorMessage)
+    {
+        errorMessage = string.Empty;
+        return true;
+    }
+}
