6c03da403326763e53dca7e445ac55a9aa8a379c
Adam Zálešák <40121923+adamzalesak@users.noreply.github.com>
2023-03-18 19:32:28 +0100

    feat: Bits & Bytes to HEX convertor (#8)
    feat: Bits & Bytes to HEX convertor

--- src/Panbyte.App/Convertors/BitsToHexConvertor.cs
+++ src/Panbyte.App/Convertors/BitsToHexConvertor.cs
@@ -4,17 +4,74 @@ namespace Panbyte.App.Convertors;
 
 public class BitsToHexConvertor : Convertor
 {
-    public BitsToHexConvertor(ConvertorOptions convertorOptions, IByteValidator byteValidator) : base(convertorOptions, byteValidator)
+    private readonly string _padding;
+
+    public BitsToHexConvertor(ConvertorOptions convertorOptions, IByteValidator byteValidator) : base(convertorOptions,
+        byteValidator)
     {
+        if (_convertorOptions.FromOptions.Count > 1 ||
+            (_convertorOptions.FromOptions.Count == 1 && (_convertorOptions.FromOptions.First() != "left" &&
+                                                          _convertorOptions.FromOptions.First() != "right")))
+        {
+            throw new ArgumentException("Invalid from-options value");
+        }
+
+        _padding = _convertorOptions.FromOptions.FirstOrDefault() ?? "left";
     }
 
     public override void ConvertPart(byte[] source, Stream destination)
     {
-        throw new NotImplementedException();
+        source = HandlePadding(source);
+
+        var bytes = GetBytes(source);
+
+        var hex = BitConverter.ToString(bytes.ToArray());
+        hex = hex.Replace("-", "").ToLower();
+        var byteArray = System.Text.Encoding.ASCII.GetBytes(hex);
+
+        destination.Write(byteArray, 0, byteArray.Length);
     }
 
-    public override bool ValidateOptions(out string errorMessage)
+
+    private byte[] HandlePadding(byte[] source)
     {
-        throw new NotImplementedException();
+        if (source.Length % 8 != 0 && _padding == "left")
+        {
+            var padding = new byte[8 - (source.Length % 8)];
+            padding = padding.Select(_ => (byte)'0').ToArray();
+            source = padding.Concat(source).ToArray();
+        }
+        else if (source.Length % 8 != 0 && _padding == "right")
+        {
+            var padding = new byte[8 - (source.Length % 8)];
+            padding = padding.Select(_ => (byte)'0').ToArray();
+            source = source.Concat(padding).ToArray();
+        }
+
+        return source;
+    }
+
+    private static List<byte> GetBytes(byte[] source)
+    {
+        var bytes = new List<byte>();
+        var bits = new List<byte>();
+
+        foreach (var bit in source)
+        {
+            bits.Add(bit == '0' ? (byte)0 : (byte)1);
+            if (bits.Count == 8)
+            {
+                var byteValue = 0;
+                for (var i = 0; i < 8; i++)
+                {
+                    byteValue += bits[i] == 0 ? 0 : (int)Math.Pow(2, 7 - i);
+                }
+
+                bytes.Add((byte)byteValue);
+                bits.Clear();
+            }
+        }
+
+        return bytes;
     }
 }
--- src/Panbyte.App/Convertors/BytesToHexConvertor.cs
+++ src/Panbyte.App/Convertors/BytesToHexConvertor.cs
@@ -4,12 +4,19 @@ namespace Panbyte.App.Convertors;
 
 public class BytesToHexConvertor : Convertor
 {
-    public BytesToHexConvertor(ConvertorOptions convertorOptions, IByteValidator byteValidator) : base(convertorOptions, byteValidator)
+    public BytesToHexConvertor(ConvertorOptions convertorOptions, IByteValidator byteValidator) : base(convertorOptions,
+        byteValidator)
     {
     }
 
     public override void ConvertPart(byte[] source, Stream destination)
     {
-        throw new NotImplementedException();
+        var hex = BitConverter.ToString(source);
+        hex = hex.Replace("-", "");
+        var bytes = System.Text.Encoding.ASCII.GetBytes(hex);
+
+        destination.Write(bytes, 0, bytes.Length);
+
+        destination.Flush();
     }
 }
