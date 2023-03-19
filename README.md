6e563fd36da88e12e15616bcf3f149b4a7bfdf05
Filip Hájek <filip.hajek@notino.com>
2023-03-19 23:07:44 +0100

    add report

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Panbyte
 
-TODO
+## Authors
+
+Adam Zálešák, 493071 \
+Filip Hájek, 493341 \
+Katarína Platková, 493144
--- src/Panbyte.App/Validators/DefaultValidator.cs
+++ src/Panbyte.App/Validators/DefaultValidator.cs
@@ -4,11 +4,6 @@ public class DefaultValidator : IByteValidator
 {
     public ByteValidation ValidateByte(byte b)
     {
-        char c = (char)b;
-        if (char.IsWhiteSpace(c))
-        {
-            return ByteValidation.Ignore;
-        }
-        return ByteValidation.Valid;
+        return b == 10 || b == 13 ? ByteValidation.Ignore : ByteValidation.Valid;
     }
 }
