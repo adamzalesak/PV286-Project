1ca29ae583985bf0a56add671ec45fc35175448c
Filip Hájek <filip.hajek@notino.com>
2023-03-19 12:45:46 +0100

    test fix 2

--- src/Panbyte.App/Program.cs
+++ src/Panbyte.App/Program.cs
@@ -2,78 +2,67 @@
 using Panbyte.App.Parser;
 using Panbyte.App.Services;
 
+ArgumentParser parser;
+StreamService streamService = new();
 
 try
 {
-    ArgumentParser parser;
-    StreamService streamService = new();
-
-    try
-    {
-        parser = new ArgumentParser(args);
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-        return -1;
-    }
-
-    if (parser.IsHelpOptionProvided())
-    {
-        PrintHelp();
-        return 1;
-    }
-
-    var parserResult = parser.Parse();
-    if (!parserResult.Success)
-    {
-        Console.WriteLine(parserResult.ErrorMessage);
-        return -2;
-    }
-
-    var (input, output) = parserResult.GetInputOutput();
-
-    if (!streamService.Exists(input))
-    {
-        Console.WriteLine($"Input file '{input}' was not found");
-        return -3;
-    }
-
-    if (!streamService.Exists(output))
-    {
-        Console.WriteLine($"Output file '{output}' was not found");
-        return -3;
-    }
-
-    var convertor = parserResult.TryCreateConvertor();
-
-    try
-    {
-        using var sourceStream = streamService.OpenInputStream(input);
-        using var outputStream = streamService.OpenOutputStream(output);
-        convertor.Convert(sourceStream, outputStream);
-        streamService.Save(outputStream);
-    }
-    catch (InvalidFormatCharacter ex)
-    {
-        Console.WriteLine($"{ex.Message}");
-        return -6;
-    }
-    catch
-    {
-        Console.WriteLine("Unknown error");
-        return -4;
-    }
-
-    return 0;
+    parser = new ArgumentParser(args);
 }
 catch (Exception ex)
 {
     Console.WriteLine(ex.Message);
-    Console.WriteLine(ex.StackTrace);
-    Console.WriteLine(ex.ToString());
-    return -100;
+    return 1;
 }
+
+if (parser.IsHelpOptionProvided())
+{
+    PrintHelp();
+    return 0;
+}
+
+var parserResult = parser.Parse();
+if (!parserResult.Success)
+{
+    Console.WriteLine(parserResult.ErrorMessage);
+    return 2;
+}
+
+var (input, output) = parserResult.GetInputOutput();
+
+if (!streamService.Exists(input))
+{
+    Console.WriteLine($"Input file '{input}' was not found");
+    return 3;
+}
+
+if (!streamService.Exists(output))
+{
+    Console.WriteLine($"Output file '{output}' was not found");
+    return 3;
+}
+
+var convertor = parserResult.TryCreateConvertor();
+
+try
+{
+    using var sourceStream = streamService.OpenInputStream(input);
+    using var outputStream = streamService.OpenOutputStream(output);
+    convertor.Convert(sourceStream, outputStream);
+    streamService.Save(outputStream);
+}
+catch (InvalidFormatCharacter ex)
+{
+    Console.WriteLine($"{ex.Message}");
+    return 6;
+}
+catch
+{
+    Console.WriteLine("Unknown error");
+    return 4;
+}
+
+return 0;
 
 static void PrintHelp()
 {
