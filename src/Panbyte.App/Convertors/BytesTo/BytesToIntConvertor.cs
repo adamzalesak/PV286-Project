401fadaf8ef69b771269e0f240de45d2a59a40d6
Adam Zálešák <adam.zalesak@notino.com>
2023-04-01 23:55:08 +0200

    feat: int convertors

---
+++ src/Panbyte.App/Convertors/BytesTo/BytesToIntConvertor.cs
@@ -0,0 +1,37 @@
+namespace Panbyte.App.Convertors.BytesTo;
+
+public class BytesToIntConvertor : IConvertor
+{
+    private readonly bool _littleEndianOutput;
+
+    public BytesToIntConvertor(ConvertorOptions convertorOptions)
+    {
+        _littleEndianOutput = convertorOptions.OutputOption == "little";
+    }
+
+    public void ConvertPart(byte[] source, Stream destination)
+    {
+        if (source.Length > 4)
+        {
+            throw new ArgumentException("Input is too long to be converted to an integer.");
+        }
+
+        if (source.Length < 4)
+        {
+            var newBytes = new byte[4];
+            Array.Copy(source, newBytes, source.Length);
+            source = newBytes;
+        }
+
+        if (!_littleEndianOutput)
+        {
+            Array.Reverse(source);
+        }
+
+        var result = BitConverter.ToUInt32(source);
+        var resultString = result.ToString();
+        var resultBytes = System.Text.Encoding.ASCII.GetBytes(resultString);
+
+        destination.Write(resultBytes);
+    }
+}
--- src/Panbyte.App/Convertors/ConvertorFactory.cs
+++ src/Panbyte.App/Convertors/ConvertorFactory.cs
@@ -2,6 +2,7 @@
 using Panbyte.App.Convertors.BitsTo;
 using Panbyte.App.Convertors.BytesTo;
 using Panbyte.App.Convertors.HexTo;
+using Panbyte.App.Convertors.IntTo;
 using Panbyte.App.Parser;
 
 namespace Panbyte.App.Convertors;
@@ -11,18 +12,25 @@ public static class ConvertorFactory
     private static readonly Dictionary<(Format From, Format To), Func<ICollection<string>, ICollection<string>, IConvertor>> factory = new()
     {
         // bits to x
-        { (Format.Bits, Format.Bits), (inputs, _) => new CopyConvertor()},
+        { (Format.Bits, Format.Bits), (_, _) => new CopyConvertor()},
         { (Format.Bits, Format.Bytes), (inputs, _) => new BitsToBytesConvertor(new ConvertorOptions(inputs.FirstOrDefault("left")))},
         { (Format.Bits, Format.Hex), (inputs, _) => new CommonConvertor(new BitsToBytesConvertor(new ConvertorOptions(inputs.FirstOrDefault("left"))), new BytesToHexConvertor(true))},
         // bytes to x
         { (Format.Bytes, Format.Bytes), (_, _) => new CopyConvertor()},
         { (Format.Bytes, Format.Hex), (_, _) => new BytesToHexConvertor()},
         { (Format.Bytes, Format.Bits), (_, _) => new BytesToBitsConvertor()},
+        { (Format.Bytes, Format.Int), (_, outputs) => new BytesToIntConvertor(new ConvertorOptions("", outputs.FirstOrDefault("big")))},
         // hex to x
         { (Format.Hex, Format.Hex), (_, _) => new CopyConvertor()},
         { (Format.Hex, Format.Bytes), (_, _) => new HexToBytesConvertor()},
         { (Format.Hex, Format.Bits), (_, _) => new CommonConvertor(new HexToBytesConvertor(), new BytesToBitsConvertor())},
         { (Format.Hex, Format.Array), (_, _) => new HexToArrayConvertor()},
+        { (Format.Hex, Format.Int), (_, outputs) => new CommonConvertor(new HexToBytesConvertor(), new BytesToIntConvertor(new ConvertorOptions("", outputs.FirstOrDefault("big"))))},
+        // int to x
+        { (Format.Int, Format.Int), (_, _) => new CopyConvertor()},
+        { (Format.Int, Format.Bytes), (inputs, _) => new IntToBytesConvertor(new ConvertorOptions(inputs.FirstOrDefault("big")))},
+        { (Format.Int, Format.Bits), (inputs, _) => new CommonConvertor(new IntToBytesConvertor(new ConvertorOptions(inputs.FirstOrDefault("big"))), new BytesToBitsConvertor())},
+        { (Format.Int, Format.Hex), (inputs, _) => new CommonConvertor(new IntToBytesConvertor(new ConvertorOptions(inputs.FirstOrDefault("big"))), new BytesToHexConvertor())},
         // array to x
         { (Format.Array, Format.Array), (_, outputs) => new ArrayToArrayConvertor(new ArrayConvertorOptions(outputs.ToArray(), "", "", Format.Array))},
         { (Format.Array, Format.Hex), (inputs, _) => new ArrayToXConvertor(new ArrayConvertorOptions(Array.Empty<string>(), inputs.FirstOrDefault("left"), "", Format.Hex))},
