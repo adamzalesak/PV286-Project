5ed9d30000002c68b83f96d61c157431f5801bb8
Katarína Platková <katkap253@gmail.com>
2023-04-08 09:46:58 +0200

    add convertor tests

---
+++ tests/Panbyte.Tests/UnitTests/ConvertorTests/ConvertorFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿using Panbyte.App.Convertors;
+using Panbyte.App.Convertors.HexTo;
+using Panbyte.App.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Panbyte.Tests.UnitTests.ConvertorTests
+{
+    public class ConvertorFactoryTests
+    {
+        [Fact]
+        public void Create_WhenValidFormats_ReturnsValidConvertor()
+        {
+            var convertor = ConvertorFactory.Create(Format.Hex, Format.Bytes, new List<string>(), new List<string>());
+            Assert.True(convertor.GetType() == typeof(HexToBytesConvertor));
+        }
+    }
+}
